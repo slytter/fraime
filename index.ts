ac12b53a5f0f3f747d308329a17b874fcda2f5b2
Schlüter <nikolaj.sn@hotmail.com>
2023-09-11 21:05:07 +0200

    Add test video and ffmpeg setup

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+var ffmpeg = require('ffmpeg');
+
+try {
+	var process = new ffmpeg('./video.mp4');
+	process.then(function (video) {
+		video.addCommand('-ss', '00:01:30')
+		video.addCommand('-vframes', '1')
+		video.save('./test.jpg', function (error, file) {
+			if (!error)
+				console.log('Video file: ' + file);
+		});
+	}, function (err) {
+		console.log('Error: ' + err);
+	});
+} catch (e) {
+	console.log(e.code);
+	console.log(e.msg);
+}
